0cb8a6bffb53a52a753e4dc0f35b72ba2954b362
nbgitpuller <nbgitpuller@nbgitpuller.link>
2025-02-24 03:53:09 +0000

    Automatic commit by nbgitpuller

--- Assignments/2025/Sem1/Assignment1/gui.py
+++ Assignments/2025/Sem1/Assignment1/gui.py
@@ -43,7 +43,8 @@ class AircraftIllustration :
     wing_vertices = [(-55,26), (7,28), (10,27), (11,28), (11,30), (8,32), (1,36), (-11,35), (-18,35), (-55,26)]        
     
     def __init__(self) : 
-        plt.ion()
+        #plt.ion()
+        plt.ioff()
         self.fig, ax = plt.subplots()
         ax.set_xlim(-200, 200)
         ax.set_ylim(-200, 200)
